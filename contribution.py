efcb1f9621fe3eb0001623cdabb5d176291a76e9
setsunayyw <yangyiwen.sigo@hotmail.com>
2021-11-17 00:25:11 +0800

    Improve plot

--- contribution.py
+++ contribution.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import os
 import datetime
+import matplotlib as mpl
+from svgpathtools import svg2paths
+from svgpath2mpl import parse_path
 from matplotlib import pyplot as plt
 
 def contributionPlot(date, activity_observed, by = "month", save = "./"):
@@ -30,8 +33,13 @@ def contributionPlot(date, activity_observed, by = "month", save = "./"):
 	elif (len(activity) > days):
 		activity = activity[-days:]
 	for i in range(row * 7 - days):
-		activity = np.append(activity, 0)
+		activity = np.append(activity, -100000)
 
+	planet_path, attributes = svg2paths("./img/rounded.svg")
+	planet_marker = parse_path(attributes[0]['d'])
+	planet_marker.vertices -= planet_marker.vertices.mean(axis=0)
+	planet_marker = planet_marker.transformed(mpl.transforms.Affine2D().rotate_deg(180))
+	planet_marker = planet_marker.transformed(mpl.transforms.Affine2D().scale(-1,1))
 	x0 = np.zeros(7, dtype = int)
 	y0 = np.arange(7)[::-1]
 	x = x0
@@ -41,13 +49,15 @@ def contributionPlot(date, activity_observed, by = "month", save = "./"):
 		y = np.append(y, y0)
 	# print(x)
 	# print(y)
-	c = activity#np.random.randint(100, size = 35)
+	c = activity#np.random.randint(100, size = 371)#
 	plt.figure(figsize = (fig_len, 4))
 	plt.style.use('dark_background')
-	plt.scatter(x = x, y = y, c = c, s = 700, marker = ",", cmap = "bone")
+	plt.scatter(x = x, y = y, c = c, s = 700, marker = planet_marker, cmap = "bone", vmin = -max(c) * 0.1, vmax = max(c))
 	# plt.text(-10, 6.5, "June", fontsize = 20)
 	# plt.colorbar()
 	plt.axis("off")
+	plt.xlim([-1, row])
+	plt.ylim([-1, 7])
 	plt.savefig(png_path)
 	if (save == "temp"):
 		os.system("gthumb " + png_path)
